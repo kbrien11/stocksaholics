8b70eabe18399cd81b6e11de40bc1f55a850c639
Jordan Marcus <jormarcus92@gmail.com>
2020-11-28 18:23:54 -0500

    Use to node-sass

--- src/App.js
+++ src/App.js
@@ -8,21 +8,21 @@ import Crypto from './components/Crypto';
 import History from './components/History';
 import About from './components/About';
 import { BrowserRouter, Route } from 'react-router-dom';
-import './App.css';
+import './styles/app.scss';
 import Landing from './components/LandingPage';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className='App'>
-        <Route exact path='/' component={Landing} />
-        <Route exact path='/signup' component={Signup} />
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/home' component={Home} />
-        <Route exact path='/stocks' component={Stocks} />
-        <Route exact path='/about' component={About} />
-        <Route exact path='/crypto' component={Crypto} />
-        <Route exact path='/history' component={History} />
+      <div className="App">
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/stocks" component={Stocks} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/crypto" component={Crypto} />
+        <Route exact path="/history" component={History} />
       </div>
     </BrowserRouter>
   );
